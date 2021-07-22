1e623d22bab99ca9dbcc2e0469c61aa6ede1b1ce
Lexy <xixi5251@gmail.com>
2021-07-21 23:12:38 -0400

    two sum

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import com.company.questions.TwoSum;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] arr1 = {2,7,11,15};
+        int target = 9;
+
+        TwoSum.twoSum(arr1, target);
+    }
+}
